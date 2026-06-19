4be3fa8bb9b9e6e67f0b2a652dddff1336099031
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2026-06-19 08:54:14 +0530

    some python basics

---
+++ python-refresher/functions.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    return a+b
+
+print(add(1, 2))
+
+# Variable length argument
+def add2(*args):
+    return sum(args)
+
+# specify the return type
+def say_hello() -> str:
+    return "Hello World"
+print(say_hello())
+
+
+def get_max(highest_number:int, lowest_number:int) -> int:
+    return max(lowest_number, highest_number)
+
+print(get_max(44, 22))
+
+#specify the parameter names with different param location
+print(get_max(lowest_number=55, highest_number=100))
+
+def get_person(firstname:str, lastname: str, age: int) -> dict:
+    return {
+        "firstname": firstname,
+        "lastname": lastname,
+        "age": age
+    }
+
+person =get_person(firstname="John", lastname="Smith", age=30)
+print(person.get("firstname"))
+
+
---
+++ python-refresher/refresher.py
@@ -0,0 +1,48 @@
+# Create a list of 5 animals called zoo
+# - Delete the animal at the 3rd index.
+# - Append a new animal at the end of the list
+# - Delete the animal at the beginning of the list.
+# - Print all the animals
+# - Print only the first 3 animals
+
+zoo = ["Lion", "Monkey", "Elephant", "Tiger", "Deer"]
+zoo.pop(2)
+zoo.append("Penguin")
+zoo.pop(0)
+
+print(zoo)
+print(zoo[0:3])
+
+# Dictionaries in python
+
+
+# Based on the dictionary:
+# my_vehicle = {
+#     "model": "Ford",
+#     "make": "Explorer",
+#     "year": 2018,
+#     "mileage": 40000
+# }
+# - Create a for loop to print all keys and values
+# - Create a new variable vehicle2, which is a copy of my_vehicle
+# - Add a new key 'number_of_tires' to the vehicle2 variable that is equal to 4
+# - Delete the mileage key and value from vehicle2
+# - Print just the keys from vehicle2
+
+my_vehicle = {
+    "model": "Ford",
+    "make": "Explorer",
+    "year": 2018,
+    "mileage": 40000
+}
+
+
+for k,v in my_vehicle.items():
+    print(f"{k}: {v}")
+
+vehicle2 = my_vehicle.copy()
+vehicle2["number_of_tires"] = 4
+vehicle2.pop("mileage")
+
+for k in vehicle2:
+    print(k)
